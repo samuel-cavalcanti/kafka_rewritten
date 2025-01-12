4cb1fb45264105c4b04fd204370af2463a130733
samuel-cavalcanti <scavalcanti111@gmail.com>
2025-01-12 16:45:31 -0300

    x

--- app/main.py
+++ app/main.py
@@ -1,4 +1,33 @@
 import socket  # noqa: F401
+from dataclasses import dataclass
+
+
+@dataclass
+class HeaderRequest:
+    api_key: int
+    api_version: int
+    correlation_id: int
+
+
+def correlation_id_respnse(id: int, msg_size: int) -> bytes:
+    # The correlation_id field is a 32-bit signed integer.
+    # The message_size field is a 32-bit signed integer
+    bytes_len = 4  # 32/4
+    response_bytes = msg_size.to_bytes(bytes_len) + id.to_bytes(bytes_len)
+    return response_bytes
+
+
+def parse_request_bytes(data: bytes) -> HeaderRequest:
+    msg_size = data[:4]
+    api_key = data[4:6]
+    api_version = data[6:8]
+    correlation_id = data[8 : 8 + 4]
+
+    return HeaderRequest(
+        int.from_bytes(api_key),
+        int.from_bytes(api_version),
+        int.from_bytes(correlation_id),
+    )
 
 
 def main():
@@ -13,16 +42,12 @@ def main():
 
     data = client_socket.recv(1028)
     print("input", data, len(data))
-    #The correlation_id field is a 32-bit signed integer.
-    correlation_id = 7
-    # The message_size field is a 32-bit signed integer
+    header = parse_request_bytes(data)
     mensage_size = 0
 
-    bytes_len = 4 # 32/4
-
-
-
-    response_bytes = mensage_size.to_bytes(bytes_len) + correlation_id.to_bytes(bytes_len)
+    response_bytes = correlation_id_respnse(
+        id=header.correlation_id, msg_size=mensage_size
+    )
 
     print("output", response_bytes, len(response_bytes))
     client_socket.send(response_bytes)
