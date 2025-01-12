15630c31a0fbe9170d163fd460e55a6e18e9d140
samuel-cavalcanti <scavalcanti111@gmail.com>
2025-01-12 16:10:38 -0300

    x

--- app/main.py
+++ app/main.py
@@ -9,7 +9,23 @@ def main():
     # Uncomment this to pass the first stage
     #
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
+    client_socket, address = server.accept()  # wait for client
+
+    data = client_socket.recv(1028)
+    print("input", data, len(data))
+    #The correlation_id field is a 32-bit signed integer.
+    correlation_id = 7
+    # The message_size field is a 32-bit signed integer
+    mensage_size = 0
+
+    bytes_len = 4 # 32/4
+
+
+
+    response_bytes = mensage_size.to_bytes(bytes_len) + correlation_id.to_bytes(bytes_len)
+
+    print("output", response_bytes, len(response_bytes))
+    client_socket.send(response_bytes)
 
 
 if __name__ == "__main__":
